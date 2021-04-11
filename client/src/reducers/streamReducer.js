1d51de86cf1886dd213f63f1472153ff9aed425d
Selcuk Cabuk <cabuk.selcuk@gmail.com>
2021-04-11 19:32:53 +0200

    add reducers

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,20 @@
+import _ from 'lodash'
+
+import { CREATE_STREAM, FETCH_STREAM, FETCH_STREAMS, EDIT_STREAM, DELETE_STREAM } from "../actions/types";
+
+export default (state={}, action) => {
+    switch (action.type) {
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case DELETE_STREAM:
+            // omit creates a new object, and delete the record in second argument
+            return _.omit(state, action.payload)
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id') }
+        default: return state
+    }
+}
