5468cadbebd9335fb4020de2eb099b45d042f72e
Selcuk Cabuk <cabuk.selcuk@gmail.com>
2021-04-11 08:53:18 +0200

    add redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,17 @@
 import 'semantic-ui-css/semantic.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-    <App />, document.querySelector('#root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
 )
