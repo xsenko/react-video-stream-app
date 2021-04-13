d0aa2ad27aa2fd9a6ce3f0ce3408b88efafb6f0c
Selcuk Cabuk <cabuk.selcuk@gmail.com>
2021-04-13 21:13:48 +0200

    add Modal and StreamDelete

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,17 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux'
-import { fetchStream} from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
 
     componentDidMount() {
-        this.props.fetchStreams(this.props.match.params.id);
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues);
     }
 
     render(){
@@ -15,7 +21,8 @@ class StreamEdit extends React.Component {
         else{
             return(
                 <div>
-                    StreamEdit
+                    <h3>Edit a Stream</h3>
+                    <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit} />
                 </div>
             )
         }
@@ -30,4 +37,4 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
